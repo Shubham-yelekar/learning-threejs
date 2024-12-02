fda9de2d936a54138ed30e3cb3fa53a28e50d59b
shubham <shubhamyelekar0@gmail.com>
2024-12-02 16:33:52 +0530

    transformation complete

--- 04-transform-objects/src/script.js
+++ 04-transform-objects/src/script.js
@@ -9,10 +9,44 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const group = new THREE.Group()
+group.scale.y = 2
+group.rotation.y = 0.2
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+cube1.position.x = - 1.5
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+cube2.position.x = 0
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+cube3.position.x = 1.5
+group.add(cube3)
+
+// Positions
+// mesh.position.x = 1;
+// mesh.position.y = 1;
+// mesh.position.z = 1;
+// mesh.position.set(0.7, -0.5, 2)
+
+// Scale
+// mesh.scale.set(0.7, -0.5, 1)
+
+// Rotation
+// mesh.rotation.x = Math.PI * 0.2;
+
 
 /**
  * Sizes
@@ -26,7 +60,7 @@ const sizes = {
  * Camera
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
+camera.position.z = 4
 scene.add(camera)
 
 /**
