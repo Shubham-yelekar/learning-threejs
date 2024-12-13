177de313ee9dfdf7a2b598cd3398cb4174dff9d8
shubham <shubhamyelekar0@gmail.com>
2024-12-13 15:29:00 +0530

    portfolio done

--- 19-scroll-animation/src/script.js
+++ 19-scroll-animation/src/script.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import GUI from "lil-gui";
+import gsap from "gsap";
 
 /**
  * Debug
@@ -12,6 +13,7 @@ const parameters = {
 
 gui.addColor(parameters, "materialColor").onChange(() => {
   material.color.set(parameters.materialColor);
+  particlesMaterial.color.set(parameters.materialColor);
 });
 
 /**
@@ -25,22 +27,61 @@ const scene = new THREE.Scene();
 
 const textureLoader = new THREE.TextureLoader();
 const gradientTexture = textureLoader.load("./textures/gradients/3.jpg");
+gradientTexture.magFilter = THREE.NearestFilter;
 
 const material = new THREE.MeshToonMaterial({
   color: parameters.materialColor,
   gradientMap: gradientTexture,
 });
 
+// Mesh
+const objectsDistance = 4;
+
 const mesh1 = new THREE.Mesh(new THREE.TorusGeometry(1, 0.4, 16, 60), material);
 
 const mesh2 = new THREE.Mesh(new THREE.ConeGeometry(1, 2, 32), material);
 
 const mesh3 = new THREE.Mesh(
-  new THREE.TorusGeometry(0.8, 0.35, 100, 16),
+  new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16),
   material
 );
+mesh1.position.y = 0;
+mesh2.position.y = -objectsDistance * 1;
+mesh3.position.y = -objectsDistance * 2;
+mesh1.position.x = 2;
+mesh2.position.x = -1;
+mesh3.position.x = 2;
 
 scene.add(mesh1, mesh2, mesh3);
+
+const sectionMeshes = [mesh1, mesh2, mesh3];
+
+// Particles
+const particlesCount = 400;
+const positions = new Float32Array(particlesCount * 3);
+
+for (let i = 0; i < particlesCount; i++) {
+  positions[i * 3] = (Math.random() - 0.5) * 10;
+  positions[i * 3 + 1] =
+    objectsDistance * 0.5 -
+    Math.random() * objectsDistance * sectionMeshes.length;
+  positions[i * 3 + 2] = (Math.random() - 0.5) * 10;
+}
+
+const particlesGeometry = new THREE.BufferGeometry();
+particlesGeometry.setAttribute(
+  "position",
+  new THREE.BufferAttribute(positions, 3)
+);
+
+const particlesMaterial = new THREE.PointsMaterial({
+  color: parameters.materialColor,
+  sizeAttenuation: true,
+  size: 0.03,
+});
+
+const particles = new THREE.Points(particlesGeometry, particlesMaterial);
+scene.add(particles);
 
 // Lights
 const directionalLight = new THREE.DirectionalLight("#fff", 3);
@@ -80,7 +121,9 @@ const camera = new THREE.PerspectiveCamera(
   100
 );
 camera.position.z = 6;
-scene.add(camera);
+const cameraGroup = new THREE.Group();
+scene.add(cameraGroup);
+cameraGroup.add(camera);
 
 /**
  * Renderer
@@ -95,11 +138,60 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 /**
  * Animate
  */
+
+// scroll
+let scrollEventY = window.scrollY;
+let currentSection = 0;
+window.addEventListener("scroll", () => {
+  scrollEventY = window.scrollY;
+  const newSection = Math.round(scrollEventY / sizes.height);
+  if (newSection !== currentSection) {
+    currentSection = newSection;
+    gsap.to(sectionMeshes[currentSection].rotation, {
+      duration: 1.5,
+      ease: "power2.inOut",
+      x: "+=6",
+      y: "+=3",
+      z: "+=1.5",
+    });
+  }
+  console.log(newSection);
+});
+
+// Mouse
+let mouseEvent = {};
+mouseEvent.x = 0;
+mouseEvent.y = 0;
+
+window.addEventListener("mousemove", (event) => {
+  mouseEvent.x = event.clientX / sizes.width - 0.5;
+  mouseEvent.y = event.clientY / sizes.height - 0.5;
+});
+
 const clock = new THREE.Clock();
+let previousTime = 0;
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
 
+  // Animate Camera
+  camera.position.y = -(scrollEventY / sizes.height) * objectsDistance;
+  const deltaTime = elapsedTime - previousTime;
+  previousTime = elapsedTime;
+
+  const parallaxX = -mouseEvent.x;
+  const parallaxY = mouseEvent.y;
+
+  cameraGroup.position.x +=
+    (parallaxX - cameraGroup.position.x) * 5 * deltaTime;
+  cameraGroup.position.y +=
+    (parallaxY - cameraGroup.position.y) * 5 * deltaTime;
+
+  // Animate meshes
+  for (const mesh of sectionMeshes) {
+    mesh.rotation.x += deltaTime * 0.1;
+    mesh.rotation.y += deltaTime * 0.15;
+  }
   // Render
   renderer.render(scene, camera);
 
