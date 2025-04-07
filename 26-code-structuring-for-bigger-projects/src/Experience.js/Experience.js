7db1c76d75968ccb53c17abd07e9d47333cefc74
shubham <shubhamyelekar0@gmail.com>
2025-04-07 20:27:14 +0530

    new set

--- 26-code-structuring-for-bigger-projects/src/Experience.js/Experience.js
+++ 26-code-structuring-for-bigger-projects/src/Experience.js/Experience.js
@@ -1,39 +1,42 @@
-import * as THREE from 'three'
-import Sizes from "./Utils/Sizes"
-import Time from "./Utils/Time"
-import Camera from './Camera'
+import * as THREE from "three";
+import Sizes from "./Utils/Sizes";
+import Time from "./Utils/Time";
+import Camera from "./Camera";
+
+let instance = null;
 
 export default class Experience {
-    constructor(canvas) {
-        // global access 
-        window.experience = this
-
-        // options
-        this.canvas = canvas
-
-        //Setup
-        this.sizes = new Sizes()
-        this.time = new Time()
-        this.scene = new THREE.Scene()
-        this.camera = new Camera()
-
-        this.sizes.on('resize', ()=>{
-            this.resize()
-        })
-
-        // Time tick event
-        this.time.on('tick', () =>
-            {
-                this.update()
-            })
+  constructor(canvas) {
+    // Singleton
+    if (instance) {
+      return instance;
     }
+    instance = this;
+    // global access
+    window.experience = this;
 
-    resize() {
-        console.log('resize');
-    }
+    // options
+    this.canvas = canvas;
 
-    update()
-    {
+    //Setup
+    this.sizes = new Sizes();
+    this.time = new Time();
+    this.scene = new THREE.Scene();
+    this.camera = new Camera();
 
-    }
+    this.sizes.on("resize", () => {
+      this.resize();
+    });
+
+    // Time tick event
+    this.time.on("tick", () => {
+      this.update();
+    });
+  }
+
+  resize() {
+    console.log("resize");
+  }
+
+  update() {}
 }
