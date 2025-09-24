6d0de1299268a77d3bc085669fc2a3690d83e46e
Shubham Yelekar <70461738+Shubham-yelekar@users.noreply.github.com>
2025-09-24 18:17:16 +0530

    particle cursor lesson done

---
+++ 39-particles-cursor-animation-shader/src/shaders/particles/vertex.glsl
@@ -0,0 +1,37 @@
+uniform vec2 uResolution;
+uniform sampler2D uPictureTexture;
+uniform sampler2D uDisplacementTexture;
+varying vec3 vColor;
+attribute float aIntensity; 
+attribute float aAngle; 
+void main()
+{
+    vec3 newPosition = position;
+    float disaplacementIntensity = texture(uDisplacementTexture, uv).r;
+    disaplacementIntensity =smoothstep(0.1, .5 , disaplacementIntensity);
+    vec3 displacement = vec3(
+        cos(aAngle) * 0.3,
+        sin(aAngle) * 0.3,
+        1.0
+    );
+    displacement = normalize(displacement);
+    displacement*= disaplacementIntensity;
+    displacement*= 3.0;
+    displacement *= aIntensity;
+    newPosition += displacement;
+    // Final position
+    vec4 modelPosition = modelMatrix * vec4(newPosition, 1.0);
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectedPosition = projectionMatrix * viewPosition;
+    gl_Position = projectedPosition;
+
+    float pictureIntensity = texture(uPictureTexture, uv).r;
+    // Point size
+    gl_PointSize = 0.15 * pictureIntensity * uResolution.y;
+    gl_PointSize *= (1.0 / - viewPosition.z);
+
+    //varying
+    vColor = vec3(pow(pictureIntensity, 2.0));
+
+
+}
